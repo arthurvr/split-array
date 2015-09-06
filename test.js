bf4b96e0c1f52096dd3f00ac0fceb6a2a589500e
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-09-06 21:44:25 +0200

    Use AVA for testing

--- test.js
+++ test.js
@@ -1,30 +1,36 @@
 'use strict';
-var assert = require('assert');
+var test = require('ava');
 var splitArray = require('./');
 
-it('splits simple array', function () {
-	assert.deepEqual(
+test('splits simple array', function (t) {
+	t.same(
 		splitArray(['a', 'b', 'c', 'd', 'e', 'f'], 2),
 		[['a', 'b'], ['c', 'd'], ['e', 'f']]
 	);
 
-	assert.deepEqual(
+	t.same(
 		splitArray(['a', 'b', 'c', 'd', 'e', 'f'], 3),
 		[['a', 'b', 'c'], ['d', 'e', 'f']]
 	);
+
+	t.end();
 });
 
-it('handles the last item correctly', function () {
-	assert.deepEqual(
+test('handles the last item correctly', function (t) {
+	t.same(
 		splitArray(['a', 'b', 'c', 'd', 'e', 'f', 'foo'], 6),
 		[['a', 'b', 'c', 'd', 'e', 'f'], ['foo']]
 	);
+
+	t.end();
 });
 
-it('Throws on nonsense input', function () {
+test('Throws on nonsense input', function (t) {
 	['foo', function () {}, null, {}].forEach(function (input) {
-		assert.throws(function () {
+		t.throws(function () {
 			splitArray(input, 1);
 		});
 	});
+
+	t.end();
 });
